b425a03722a08b246bd76724d891c2e3417c520f
Eiríkur Heiðar Nilsson <eirikur@nilsson.is>
2013-01-26 19:56:19 +0000

    Added game boilerplate. Rewritten in js.

---
+++ game/app/scripts/player.js
@@ -0,0 +1,25 @@
+/*global define */
+
+define(['controls'], function(controls) {
+
+  var Player = function(el) {
+    this.el = el;
+    this.pos = { x: 0, y: 0 };
+    this.speed = 200;
+  };
+
+  Player.prototype.onFrame = function(delta) {
+    if (controls.spacePressed) {
+      this.pos.x += delta * this.speed;
+    }
+
+    if (this.pos.x > 200 || this.pos.x < 0) {
+      this.speed *= -1;
+    }
+
+    // Update UI
+    this.el.css('-webkit-transform', 'translate(' + this.pos.x + 'px,' + this.pos.y + 'px)');
+  };
+
+  return Player;
+});
