b450fc0b9b6fd2cd745d865ef901d4aa3964a6d7
Eiríkur Heiðar Nilsson <eirikur@nilsson.is>
2013-01-27 00:54:21 +0000

    Jumping

--- game/app/scripts/player.js
+++ game/app/scripts/player.js
@@ -3,11 +3,14 @@
 define(['controls'], function(controls) {
 
   var PLAYER_SPEED = 300;
+  var JUMP_VELOCITY = 1000;
+  var GRAVITY = 2500;
 
   var transform = $.fx.cssPrefix + 'transform';
 
   var Player = function(el) {
     this.el = el;
+    this.jumping = false;
     this.pos = { x: 0, y: 0 };
     this.vel = { x: 0, y: 0 };
   };
@@ -23,8 +26,27 @@ define(['controls'], function(controls) {
       this.vel.x = 0;
     }
 
+    // Jump
+    if (controls.keys.space && !this.jumping) {
+      this.jumping = true;
+      this.vel.y = -JUMP_VELOCITY;
+    }
+
     // Update state
     this.pos.x += this.vel.x * delta;
+    this.pos.y += this.vel.y * delta;
+
+    // Collision with the ground.
+    if (this.pos.y > 0) {
+      this.vel.y = 0;
+      this.pos.y = 0;
+      this.jumping = false;
+    }
+
+    // Gravity
+    if (this.pos.y !== 0) {
+      this.vel.y += GRAVITY * delta;
+    }
 
     // Update UI.
     this.el.css(transform, 'translate(' + this.pos.x + 'px,' + this.pos.y + 'px)');
